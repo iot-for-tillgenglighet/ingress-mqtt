502081cdbf79b2bd03159842eb13139b1cc34342
Sofia Styf <sofia.styf@masarinconsulting.se>
2020-08-20 18:53:19 +0200

    added test that checks date format

--- Masarin.IoT.Sensor.Tests/MQTTDecoderSnowdepthTests.cs
+++ Masarin.IoT.Sensor.Tests/MQTTDecoderSnowdepthTests.cs
@@ -11,18 +11,13 @@ namespace Masarin.IoT.Sensor.Tests
         [Fact]
         public void IfSummer_SnowdepthShouldNotSend()
         {
-            //Arrange          
             var mockMQ = new Mock<IMessageQueue>();
             var decoder = new MQTTDecoderSnowdepth(mockMQ.Object);
             
-            byte[] bytes = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            var data = Convert.ToBase64String(bytes);
-            var payload = "{\"data\":\"" + data + "\"}";
+            byte[] bytes = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 12, 13, 14, 15, 16, 17, 18 };
             
-            //Act
-            decoder.Decode("2020-08-19T14:22:36Z", "ff", "", Encoding.UTF8.GetBytes(payload));
+            decoder.Decode("2020-08-19T14:22:36Z", "ff", "", CreateTestPayload(bytes));
 
-            //Assert
             mockMQ.Verify(foo => foo.PostMessage(It.IsAny<TelemetrySnowdepth>()), Times.Never());
         }
         
@@ -30,19 +25,47 @@ namespace Masarin.IoT.Sensor.Tests
         [Fact]
         public void IfNotSummer_SnowdepthShouldSend()
         {
-            //Arrange          
             var mockMQ = new Mock<IMessageQueue>();
             var decoder = new MQTTDecoderSnowdepth(mockMQ.Object);
             
-            byte[] bytes = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            byte[] bytes = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 12, 13, 14, 15, 16, 17, 18 };
+
+            decoder.Decode("2020-11-19T14:22:36Z", "ff", "", CreateTestPayload(bytes));
+
+            mockMQ.Verify(foo => foo.PostMessage(It.IsAny<TelemetrySnowdepth>()), Times.Once());
+        }
+
+        [Fact]
+        public void IfDate_IsWrongFormat()
+        {
+            var mockMQ = new Mock<IMessageQueue>();
+            var decoder = new MQTTDecoderSnowdepth(mockMQ.Object);
+            
+            byte[] bytes = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 12, 13, 14, 15, 16, 17, 18 };
+
+            Assert.Throws<System.FormatException>(() => decoder.Decode("gurka", "ff", "", CreateTestPayload(bytes)));
+        }
+
+        [Fact]
+        public void IfSensor_IsNotOkay()
+        {   
+            var mockMQ = new Mock<IMessageQueue>();
+            var decoder = new MQTTDecoderSnowdepth(mockMQ.Object);
+
+            //The byte array is needed to form the payload that PostMessage sends. Without it, the test does not work.
+            //The 11th byte (11), should be 0 if the sensor is okay (see previous tests).
+            byte[] bytes = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
+
+            decoder.Decode("2020-11-19T14:22:36Z", "ff", "", CreateTestPayload(bytes));
+
+            mockMQ.Verify(foo => foo.PostMessage(It.IsAny<TelemetrySnowdepth>()), Times.Never());
+        }
+
+        private static byte[] CreateTestPayload(byte[] bytes)
+        {
             var data = Convert.ToBase64String(bytes);
             var payload = "{\"data\":\"" + data + "\"}";
-            
-            //Act
-            decoder.Decode("2020-11-19T14:22:36Z", "ff", "", Encoding.UTF8.GetBytes(payload));
-
-            //Assert
-            mockMQ.Verify(foo => foo.PostMessage(It.IsAny<TelemetrySnowdepth>()), Times.Once());
+            return Encoding.UTF8.GetBytes(payload);
         }
     }
 }
